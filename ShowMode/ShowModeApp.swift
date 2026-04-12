7158fce245ca2ced146d3c24ee880577343bdfc7
JanWillemBrands <jan.willem.brands@gmail.com>
2026-04-12 12:02:25 +0900

    Initial Commit

---
+++ ShowMode/ShowModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowModeApp.swift
+//  ShowMode
+//
+//  Created by Johannes Brands on 2026.04.12.
+//
+
+import SwiftUI
+
+@main
+struct ShowModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
